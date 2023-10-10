6edf0d68041dc1e1f5891d6a3f9880d46901f5d5
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-10 15:51:44 -0700

    Update token.h

--- src/lib/token.h
+++ src/lib/token.h
@@ -1,20 +1,39 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+
 #include <string>
 enum class TokenType{
   EXP,
   CONST,
+  NULL,
 };
 
 class Token{
-int column;
-int line;
-std::string text;
-TokenType type;
+int pcolumn;
+int pline;
+std::string ptext;
+TokenType ptype;
 
-int get_col() const { return this->column; };
-int get_line() const { return this->line; };
-const std::string& get_text() const { return this->text; };
-TokenType get_type() const { return this->type; };
+Token(int column, int line, const std::string& text, TokenType type) : pcolumn{column}, pline{line}, ptext{text}, ptype{type} {}
+int get_col() const { return this->pcolumn; };
+int get_line() const { return this->pline; };
+const std::string& get_text() const { return this->ptext; };
+TokenType get_type() const { return this->ptype; };
 };
 #endif
+/*
+
+how to respond to different token types:
+
+switch(token1.get_type()){
+case TokenType::EXP:
+  do_something();
+  break;
+case TokenType::CONST:
+  do_something_else();
+  break;
+default:
+  break;
+}
+*/
+
