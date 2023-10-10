24b72c351c85a0ff782e2ed5fbb5e2b8b1d372e1
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-10 15:33:59 -0700

    Update token.h

--- src/lib/token.h
+++ src/lib/token.h
@@ -1,3 +1,20 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <string>
+enum class TokenType{
+  EXP,
+  CONST,
+};
+
+class Token{
+int column;
+int line;
+std::string text;
+TokenType type;
+
+int get_col() const { return this->column; };
+int get_line() const { return this->line; };
+const std::string& get_text() const { return this->text; };
+TokenType get_type() const { return this->text; };
+};
 #endif
