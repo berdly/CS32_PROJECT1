9a96ce4ba1c7ca23fd06daa008e5a8759d40a4d2
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-10 16:33:21 -0700

    Update parser.h
    Abstract syntax tree setup

--- src/lib/parser.h
+++ src/lib/parser.h
@@ -1,3 +1,19 @@
 #ifndef PARSER_H
 #define PARSER_H
+
+#include <vector>
+#include "token.h"
+
+class ASTree{
+
+  struct ASnode{
+    Token pdata;
+    std::vector<ASnode> pchildren;
+  }
+
+  ASnode proot;
+
+public:
+  ASTree(const std::vector<Token>& tokens);
+}
 #endif
