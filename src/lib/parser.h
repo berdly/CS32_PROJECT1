02a59abd4002a9f06462e29eb8ee227973769547
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-11 11:49:03 -0700

    Updated headers to match parser implementation

--- src/lib/parser.h
+++ src/lib/parser.h
@@ -2,6 +2,7 @@
 #define PARSER_H
 
 #include <vector>
+#include <utility>
 #include "token.h"
 
 class ASTree{
@@ -9,17 +10,16 @@ class ASTree{
   class ASnode{
     Token pdata;
     std::vector<ASnode> pchildren;
-
-    void buildHelp(const std::vector<Token>& tokens, int idx);
-    
+    public:
     ASnode(Token pdata);
+    void add_child(ASNode child);
   }
 
   ASnode proot;
 
 public:
   ASTree(const std::vector<Token>& tokens);
-
-  
+  ASNode build(const std::vector<Token>& tokens, int start, int end);
+  std::vector<std::pair<int>> get_child_idx(const std::vector<Token>& tokens, int start, int end);
 }
 #endif
