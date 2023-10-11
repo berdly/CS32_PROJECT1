de12d5454ea9e5dfe1981c5bae4b85435a39cb05
Sawyer Rice <sawyerrice@ucsb.edu>
2023-10-10 18:52:00 -0700

    add parser.cpp

---
+++ src/parser.cpp
@@ -0,0 +1,46 @@
+#include "parser.h"
+
+
+ASTree::ASTree(const std::vector<Token>& tokens){
+
+    for(size_t i = 0 ; i < tokens.size();i++){
+        Token check = tokens.at(i);
+
+        if(i == 0){
+
+            if(check.get_type() == TokenType::CONST and tokens.size()!= 1){
+                throw ParseError("Parse error");
+            }
+
+            proot.pdata = check;
+
+            continue;
+        }
+
+        if(check.get_type() == TokenType::PAR){
+
+            proot.buildHelp(tokens, i);
+
+        }
+
+    }
+
+}
+
+
+void ASnode::buildHelp(const std::vector<Token>& tokens, int idx){
+
+    if(tokens.at(idx).get_type() == TokenType::CONST){
+        ASnode add = ASnode(tokens.at(idx))
+        self.pchildren.push_back(add);
+        
+
+    }
+
+
+}
+
+ASnode::ASnode(Token pdata){
+    self.pdata = pdata;
+    pchildren = new std::vector<ASnode>();
+}
