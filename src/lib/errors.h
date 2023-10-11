d1c89035b9eda033514bda6ee588232b7b20520a
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-11 13:34:45 -0700

    Fixed errors.h

--- src/lib/errors.h
+++ src/lib/errors.h
@@ -1,26 +1,31 @@
 #ifndef ERRORS_H
 #include <string>
+#include "token.h"
 
 class ParserError : public std::exception {
     public:
-      char* what (const Token& token) {
+        Token etoken;
+        ParserError(Token token) : etoken{token} {}
+      char* what () {
         std::string msg{"Unexpected token at line "};
-        msg.append(std::to_string(token.get_line());
+        msg.append(std::to_string(this->etoken.get_line());
         msg.append(" column ");
-        msg.append(std::to_string(token.get_col());
+        msg.append(std::to_string(this->etoken.get_col());
         msg.push_back(': ');
-        msg.append(token.get_text());
+        msg.append(this->etoken.get_text());
         return msg.c_string();
         ;
     }
 };
 class LexerError : public std::exception {
     public:
-      char* what (const Token& token) {
+        Token etoken;
+        LexerError(Token token) : etoken{token} {}
+      char* what () {
         std::string msg{"Syntax error on line "};
-        msg.append(std::to_string(token.get_line());
+        msg.append(std::to_string(this->etoken.get_line());
         msg.append(" and column ");
-        msg.append(std::to_string(token.get_col());
+        msg.append(std::to_string(this->etoken.get_col());
         msg.push_back('.');
         return msg.c_string();
         ;
