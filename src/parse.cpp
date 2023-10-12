b9e3646b7a1b32361700831d9e21a78479d9a51d
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-12 16:33:55 -0700

    First draft of parse.cpp

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
+#include <string>
+#include "lexer.h"
 #include "parser.h"
 
 
 
 int main(){
-
-
-    
+    std::string input{};
+    std::cin >> input;
+    ASTree tree{reader(input)};
+    tree.print();
+    std::cout << '\n';
+    try{
+    std::cout << tree.calc();
+    }
+    catch(const ZeroDivision&){
+        std::cout << "Runtime error: division by zero.";
+    }
 }
