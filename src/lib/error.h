cc913da7722d2e0cbbee72ba429f6ed3eeba1835
Jakob Pinedo <jakob.pinedo@gmail.com>
2023-10-14 20:38:40 -0700

    error fixes

--- src/lib/error.h
+++ src/lib/error.h
@@ -8,9 +8,9 @@ class ParserError : public std::exception {
         ParserError(Token token) : etoken{token} {}
       std::string new_what () const{
         std::string msg{"Unexpected token at line "};
-        msg.append(std::to_string(this->etoken.get_line());
+        msg.append(std::to_string(this->etoken.get_line()));
         msg.append(" column ");
-        msg.append(std::to_string(this->etoken.get_col());
+        msg.append(std::to_string(this->etoken.get_col()));
         msg.append(": ");
         msg.append(this->etoken.get_text());
         return msg;
@@ -23,9 +23,9 @@ class LexerError : public std::exception {
         LexerError(Token token) : etoken{token} {}
       std::string new_what () const{
         std::string msg{"Syntax error on line "};
-        msg.append(std::to_string(this->etoken.get_line());
+        msg.append(std::to_string(this->etoken.get_line()));
         msg.append(" and column ");
-        msg.append(std::to_string(this->etoken.get_col());
+        msg.append(std::to_string(this->etoken.get_col()));
         msg.push_back('.');
         return msg;
         ;
