a5396b3929d4083ebb45e1e4360371d3cbefd54c
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2023-10-15 19:27:49 -0700

    Update lex.cpp by moving the test code from lexer.cpp

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,3 +1,19 @@
-int main(){
+int main() {
+    std::string input;
+    int fline;
+
+    // Prompt the user for input.
+    std::cout << "Enter the expression: ";
+    std::getline(std::cin, input);
+
+    // Parse the input and get the tokens.
+    std::vector<Token> tokens = reader(input);  // Change to vector
     
+    // Display the tokens.
+    for (const Token& t : tokens) {
+        std::cout << t.get_line() << "  " << t.get_col() << "  " << t.get_text() << std::endl;
+        fline = t.get_line();
+    }
+    std::cout<<fline+1<<"   1  END";
+    return 0;
 }
