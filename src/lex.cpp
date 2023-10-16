39f12e894302868e9a7295a7ac40ef3d62c12f74
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2023-10-15 22:11:22 -0700

    Fixed it

--- src/lex.cpp
+++ src/lex.cpp
@@ -2,20 +2,29 @@
 
 int main() {
     std::string input;
-    int fline;
+    std::vector<Token> tokens;
 
     // Prompt the user for input.
     std::cout << "Enter the expression: ";
-    std::getline(std::cin, input);
-
-    // Parse the input and get the tokens.
-    std::vector<Token> tokens = reader(input);  // Change to vector
-    
-    // Display the tokens.
-    for (const Token& t : tokens) {
-        std::cout << t.get_line() << "  " << t.get_col() << "  " << t.get_text() << std::endl;
-        fline = t.get_line();
+    while (getline(std::cin, input) && !input.empty()) { // Continue taking input until an empty line is entered
+        std::vector<Token> new_tokens = reader(input);  // Parse each line and get the tokens
+        tokens.insert(tokens.end(), new_tokens.begin(), new_tokens.end()); // Add new tokens to the existing list of tokens
     }
-    std::cout<<fline+1<<"   1  END";
+
+    // Function to display the tokens with right-aligned columns.
+    for (const Token& t : tokens) {
+        std::cout << std::right << std::setw(4) << t.get_line() << " "
+                  << std::right << std::setw(4) << t.get_col() << "  "
+                  << t.get_text() << std::endl;
+    }
+
+    // Display the END token
+    if (!tokens.empty()) { // Ensure there are tokens before trying to access the last token
+        std::cout << std::right << std::setw(4) << tokens.back().get_line() + 1 
+                  << "    1  END" << std::endl;
+    } else {
+        std::cout << "   1    1  END" << std::endl; // If there are no tokens, default to line 1
+    }
+
     return 0;
 }
