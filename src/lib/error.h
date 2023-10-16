534c30540297070d4c830e4aac32a953c9438a27
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-16 13:28:38 -0700

    Update error.h

--- src/lib/error.h
+++ src/lib/error.h
@@ -1,11 +1,16 @@
 #ifndef ERRORS_H
 #include <string>
 #include "token.h"
-
+enum class PErrType{
+    NORM,
+    END,
+};
 class ParserError : public std::exception {
     public:
         Token etoken;
-        ParserError(Token token) : etoken{token} {}
+        PErrType etype;
+        ParserError(Token token) : etoken{token} etype{PErrType::NORM} {}
+        ParserError(Token token, PErrType type) : etoken{token} etype{type} {}
       std::string new_what () const{
         std::string msg{"Unexpected token at line "};
         msg.append(std::to_string(this->etoken.get_line()));
@@ -14,7 +19,6 @@ class ParserError : public std::exception {
         msg.append(": ");
         msg.append(this->etoken.get_text());
         return msg;
-        ;
     }
 };
 class LexerError : public std::exception {
