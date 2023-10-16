82ce5ae7ee1bd8a7669c1679a69b506d8697ddc4
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2023-10-16 09:42:02 -0700

    Update error.h

--- src/lib/error.h
+++ src/lib/error.h
@@ -6,14 +6,14 @@ class ParserError : public std::exception {
     public:
         Token etoken;
         ParserError(Token token) : etoken{token} {}
-      char* what () {
+      std::string new_what () const{
         std::string msg{"Unexpected token at line "};
-        msg.append(std::to_string(this->etoken.get_line());
+        msg.append(std::to_string(this->etoken.get_line()));
         msg.append(" column ");
-        msg.append(std::to_string(this->etoken.get_col());
-        msg.push_back(': ');
+        msg.append(std::to_string(this->etoken.get_col()));
+        msg.append(": ");
         msg.append(this->etoken.get_text());
-        return msg.c_str();
+        return msg;
         ;
     }
 };
@@ -21,14 +21,17 @@ class LexerError : public std::exception {
     public:
         Token etoken;
         LexerError(Token token) : etoken{token} {}
-      char* what () {
+      std::string new_what () const{
         std::string msg{"Syntax error on line "};
-        msg.append(std::to_string(this->etoken.get_line());
+        msg.append(std::to_string(this->etoken.get_line()));
         msg.append(" and column ");
-        msg.append(std::to_string(this->etoken.get_col());
+        msg.append(std::to_string(this->etoken.get_col()));
         msg.push_back('.');
-        return msg.c_str();
+        return msg;
         ;
     }
+};
+
+class ZeroDivision : public std::exception {
 };
 #endif
