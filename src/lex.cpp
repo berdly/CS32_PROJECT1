2a4cd2bc7cd9548cc0ff14902114a56997655573
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-15 20:50:49 -0700

    Update lex.cpp

--- src/lex.cpp
+++ src/lex.cpp
@@ -5,17 +5,15 @@
 #include <sstream>
 
 int main() {
-    std::string input{};
-    std::string fullstr{};
+    char* raw{new char[1000]};
+    std::cin.read(raw, 1000);
+    std::string input{raw};
+    delete raw;
+    
     int fline;
-    while (!std::cin.eof()){
-        std::getline(std::cin, input);
-        fullstr.append(input);
-        input.clear();
-    }
 
     // Parse the input and get the tokens.
-    std::vector<Token> tokens = reader(fullstr);  // Change to vector
+    std::vector<Token> tokens = reader(input);  // Change to vector
     
     // Display the tokens.
     // Use a range-based for loop to iterate over the vector
