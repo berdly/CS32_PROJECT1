fc9b184e92ef686e063223751f5e89f720649b92
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-17 16:41:07 -0700

    Lexer Var Support

--- src/lib/lexer.cpp
+++ src/lib/lexer.cpp
@@ -6,9 +6,10 @@
 // Function to parse the input string and create tokens for each number and operand.
 std::vector<Token> reader(const std::string& input) {  // Change return type to vector
     std::vector<Token> tokens;  // Use vector instead of stack to store generated tokens
-    
+    bool startsNum{false};
+    bool startsVar{false};
     // Temporary string to accumulate digits of a number.
-    std::string currentNumber;
+    std::string currToken;
     int column = 1, line = 1;
 
     for (size_t i = 0; i < input.size(); ++i) {
@@ -22,10 +23,18 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
             case '*':
             case '/':
                 // If there's an accumulated number, create a token for it.
-                if (!currentNumber.empty()) {
-                    tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
-                    currentNumber.clear();
+                if (!currToken.empty()) {
+                if(startsVar){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::VAR));
+                    currToken.clear();
+                    startsVar = false;
                 }
+                else if(startsNum){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
+                    currToken.clear();
+                    startsNum = false;
+                }
+            }
                 // Create a token for the operator.
                 tokens.push_back(Token(column, line, std::string(1, ch), TokenType::EXP));
                 break;
@@ -33,10 +42,18 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
             // For left parenthesis.
             case '(':
                 // If there's an accumulated number, create a token for it.
-                if (!currentNumber.empty()) {
-                    tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
-                    currentNumber.clear();
+                if (!currToken.empty()) {
+                if(startsVar){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::VAR));
+                    currToken.clear();
+                    startsVar = false;
                 }
+                else if(startsNum){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
+                    currToken.clear();
+                    startsNum = false;
+                }
+            }
                 // Create a token for the left parenthesis.
                 tokens.push_back(Token(column, line, std::string(1, ch), TokenType::LPAR));
                 break;
@@ -44,10 +61,18 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
             // For right parenthesis.
             case ')':
                 // If there's an accumulated number, create a token for it.
-                if (!currentNumber.empty()) {
-                    tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
-                    currentNumber.clear();
+                 if (!currToken.empty()) {
+                if(startsVar){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::VAR));
+                    currToken.clear();
+                    startsVar = false;
                 }
+                else if(startsNum){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
+                    currToken.clear();
+                    startsNum = false;
+                }
+            }
                 // Create a token for the right parenthesis.
                 tokens.push_back(Token(column, line, std::string(1, ch), TokenType::RPAR));
                 break;
@@ -55,33 +80,46 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
             // For decimal points.
             case '.':
                 // Check for multiple decimal points in the current number.
-                if (currentNumber.find('.') != std::string::npos) {
+                if(startsVar){
                    std::cout << "Syntax error on line " << line << " column " << column << "." << std::endl;
-                   currentNumber.clear();
+                   currToken.clear(
+                   exit(1);
+                }
+                if (currToken.find('.') != std::string::npos) {
+                   std::cout << "Syntax error on line " << line << " column " << column << "." << std::endl;
+                   currToken.clear();
                    exit(1);
                 }
                 // Check for trailing decimals.
                 else if (i + 1 == input.size() || isspace(input[i + 1]) || input[i + 1] == '\n') {
                     std::cout << "Syntax error on line " << line << " column " << column+1 << "." << std::endl;
-                    currentNumber.clear();
+                    currToken.clear();
                     exit(1);
                }
                 // Check for leading decimals.
                else if (currentNumber.empty()) {
                    std::cout << "Syntax error on line " << line << " column " << column << "." << std::endl;
-                   currentNumber.clear();
+                   currToken.clear();
                    exit(1);
                } else {
-                   currentNumber += ch;
+                   currToken += ch;
                 }
                break;
             
             // For newline characters.
             case '\n':
             // If there's an accumulated number, create a token for it.
-            if (!currentNumber.empty()) {
-                tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
-                currentNumber.clear();
+            if (!currToken.empty()) {
+                if(startsVar){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::VAR));
+                    currToken.clear();
+                    startsVar = false;
+                }
+                else if(startsNum){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
+                    currToken.clear();
+                    startsNum = false;
+                }
             }
                 line++;
                 column = 0;
@@ -89,22 +127,44 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
 
             case ' ':
                 // White space seperate
-                if (!currentNumber.empty()) {
-                    tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
-                    currentNumber.clear();
+                if (!currToken.empty()) {
+                if(startsVar){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::VAR));
+                    currToken.clear();
+                    startsVar = false;
                 }
+                else if(startsNum){
+                    tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
+                    currToken.clear();
+                    startsNum = false;
+                }
+            }
                 break;               
             
             // For other characters.
             default:
                 // If it's a digit, add to the current number.
                 if (isdigit(ch)) {
-                    currentNumber += ch;
+                    if(!(startsNum || startsVar)){
+                        startsNum = true;
+                    }
+                    currToken += ch;
                 } 
                 // If it's an invalid character, create an error token.
-                else if (isalpha(ch) || !isspace(ch)) {
+                else if(isalpha(ch) || ch == '_'){
+                    if(!(startsNum || startsVar)){
+                        startsVar = true;
+                    }
+                    if(startsNum){
+                        std::cout<<"Syntax error on line "<< line <<" column " << column << "." << std::endl;
+                        currToken.clear();
+                        exit(1);
+                    }
+                    currToken += ch;
+                }
+                else if (!isspace(ch)) {
                     std::cout<<"Syntax error on line "<< line <<" column " << column << "." << std::endl;
-                    currentNumber.clear();
+                    currToken.clear();
                     exit(1);
                 }
                 break;
@@ -113,8 +173,8 @@ std::vector<Token> reader(const std::string& input) {  // Change return type to 
     }
 
     // If there's any remaining number, create a token for it.
-    if (!currentNumber.empty()) {
-        tokens.push_back(Token(column - currentNumber.size(), line, currentNumber, TokenType::CONST));
+    if (!currToken.empty()) {
+        tokens.push_back(Token(column - currToken.size(), line, currToken, TokenType::CONST));
     }
 
     return tokens;
