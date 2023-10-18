94a407069808517b73bb1416c48f5b6068688a81
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-17 20:57:26 -0700

    Update grove.h

--- src/lib/grove.h
+++ src/lib/grove.h
@@ -9,6 +9,32 @@ class ASGrove<template class T>{
   public:
   T eval();
   std::map<std::string, double> getVariables();
-  
-  
 };
+/*
+#include <typeinfo>
+#include <typeid>
+#include <any>
+#include <optional>
+
+//for later, exploration into implementing a version of grove which can hold variables of ARBITRARY TYPE:)
+//having trouble with building a nice way to revert the data from std::any to the original type without knowing manually entering the original type
+//if anyone has any ideas lmk
+
+class VarDict{
+  std::map<std::string, std::pair<std::any, std::type_index>> dict;
+  
+  template<class T>
+  void add(const std::string& name, const T& data); 
+  std::pair<std::any, std::type_index> access(const std::string& 
+}
+class ASGrove<template class ReturnType>{
+  static std::set<std::type_index> valid_types;
+  std::vector<ASTree> statements;
+  VarDict variables;
+  
+  std::optional<ReturnType> step();
+  public:
+  ReturnType eval();
+  std::map<std::string, double> getVariables();
+};
+*/
