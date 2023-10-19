658dc02d9a6a1b25407d586445d34698977b41c1
Sawyer Rice <sawyerrice@ucsb.edu>
2023-10-19 14:49:47 -0700

    readded constructor

--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -1,6 +1,9 @@
 #include "parser.h"
 #include "error.h"
 #include <iostream>
+ASTree::ASTree(const std::vector<Token>& tokens) {
+    this->proot = this->build(tokens, 0, tokens.size() - 1);
+}
 
 std::vector<std::pair<int,int>> ASTree::get_child_idx(const std::vector<Token>& tokens, int start, int end){
     /*
