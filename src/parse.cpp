aa6c627bd210af8837d389ccb81105f1959c385d
Sawyer Rice <sawyerrice@ucsb.edu>
2023-10-24 08:13:31 -0700

    fixing main

--- src/parse.cpp
+++ src/parse.cpp
@@ -8,7 +8,7 @@
 
  int main(){
     std::vector<ASTree> statements{};
-    /*std::string input, line;
+    std::string input, line;
     //bool l = false;  // Initialize l to false'
     int linecount{};
     
@@ -29,7 +29,7 @@
         //l = false;
         //std::cout<<input<<std::endl;
 
-        //auto tokens{reader(input)};
+        auto tokens{reader(input)};
 
 
         try{
@@ -68,45 +68,9 @@
     for(size_t i = 0; i < statements.size();i++){
     grove.print();
     grove.eval(); 
-    } */
+    } 
   
-    try{
-    auto tokens1{reader("(= 3 4)")};
-    //auto tokens2{reader("(+ 34 x 56)")};
-    ASTree tree1{tokens1};
-    //ASTree tree2{tokens2};
-    //auto tokens2{reader(input)};
-    //auto tokens3{reader(input)};
-    statements.push_back(tree1);
-    //statements.push_back(tree2); 
-    //statements.push_back(tokens3);
     
-    ASGrove grove{statements};
-    for(size_t i = 0; i < statements.size();i++){
-    grove.print();
-    grove.eval(); 
-    }
-
-    }catch(const ParserError& e){
-            //handles parser errors and displays error messages, else block is for early endings since the END token is not on the actual vector and must be imagined next to the final token
-            if(e.etype == PErrType::NORM){
-                //std::cout<<"THORW 3";
-            std::cout << e.new_what() << '\n';
-            }
-            else{
-                std::cout << "Unexpected token at line " << e.etoken.get_line() << " column " << e.etoken.get_col() + e.etoken.get_text().size() << ": END\n";
-            }
-            return 2;
-        }
-        catch(const ZeroDivision&){
-            std::cout << "Runtime error: division by zero." << '\n';
-            return 3;
-        }
-        catch(const IdentifierError& e){
-            std::cout<<e.new_what()<<"\n";
-            return 3;
-        }
-
         
     }
 
