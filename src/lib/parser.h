0b407d0af74cf190afb8fc6b095dabc792f30a18
JLowe32 <122652186+JLowe32@users.noreply.github.com>
2023-10-25 15:13:20 -0700

    Update parser.h // Added new buildInfix Function

--- src/lib/parser.h
+++ src/lib/parser.h
@@ -6,39 +6,36 @@
 #include "token.h"
 
 class ASTree{
-public:
+
   class ASNode{
     Token pdata;
     std::vector<ASNode> pchildren;
     public:
     ASNode();
     ASNode(Token pdata);
-    const Token& get_pdata() const;
+    Token get_pdata();
 
     void add_child(ASNode child);
-    //double calcHelp();
-    //void printHelp() const;
-
-   //tree shouldn't have variable val
+    double calcHelp();
+    void printHelp();
     
-    const std::vector<ASNode>& get_kids() const;
+    std::vector<ASNode> getkids();
   };
 
-private:
+  
   ASNode proot;
-  std::vector<Token> infixToPostfix(const std::vector<Token>& tokens);
-  int getPrecedence(const Token& token);
-  std::vector<Token> postfixToInfix(const std::vector<Token>& postfixTokens);
 
 public:
   
   ASTree(const std::vector<Token>& tokens);
   ASNode build(const std::vector<Token>& tokens, int start, int end);
+  ASNode buildInfix(const std::vector<Token>& tokens);
   std::vector<std::pair<int,int>> get_child_idx(const std::vector<Token>& tokens, int start, int end);
-  //double calc();
-  //void print();
-  const ASNode& getProot() const;
-  std::vector<Token> infixWithParens(const std::vector<Token>& tokens);
+  double calc();
+  void print();
+  ASNode getProot();
+  double precendense(std::string text);
+
 };
 
 #endif
