062cf929e3006712ccf2b3ff6fbb22321e1a0bb5
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-26 16:51:49 -0700

    Update parser.h

--- src/lib/parser.h
+++ src/lib/parser.h
@@ -29,8 +29,8 @@ public:
 private:
   ASNode proot;
   ASNode build(const std::vector<Token>& tokens, int start, int end);
-  std::vector<std::pair<int,int>> get_child_idx(const std::vector<Token>& tokens, unsigned start, unsigned end);
-  ASTree::ASNode buildInfix(const std::vector<Token>&, int, int);
+  std::vector<std::pair<int,int>> get_child_idx(const std::vector<Token>& tokens, int start, int end);
+  ASTree::ASNode buildInfix(const std::vector<Token>&, unsigned, unsigned);
   double precedence(std::string text);
 
 public:
