02cdb2993842bf690d6ab26df6739ae7cc649728
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-26 18:22:41 -0700

    Update lexer.h

--- src/lib/lexer.h
+++ src/lib/lexer.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include "token.h"
 
-std::vector<Token> reader(const std::string& input);
+std::vector<Token> reader(const std::string& input, bool exit = false);
 std::vector<std::vector<Token>> split(const std::vector<Token>& input);
 
 #endif
