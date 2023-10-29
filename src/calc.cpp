898e1ab2d7b2d01a4536ed6ffe67d50374538a60
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-10-29 14:15:02 -0700

    Update calc.cpp

--- src/calc.cpp
+++ src/calc.cpp
@@ -36,7 +36,7 @@
             std::cout << e.new_what() << '\n';
             }
             else{
-                std::cout << "Unexpected token at line " << e.etoken.get_line() << " column " << e.etoken.get_col() + e.etoken.get_text().size() << ": END\n";
+                std::cout << "Unexpected token at line " << e.etoken.get_line() << " column " << e.etoken.get_col() + e.etoken.get_text().size() + 1<< ": END\n";
             }
         }
         catch(const ZeroDivision&){
