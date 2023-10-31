012342cc7c61e62c594a300802a523cd6525b9e9
dcbang747 <31807533+dcbang747@users.noreply.github.com>
2023-10-30 19:14:07 -0700

    Added Function Descriptions

--- src/lib/grove.h
+++ src/lib/grove.h
@@ -20,7 +20,7 @@ PRINT,
 
 class ASGrove{
   std::vector<ASTree*> statements;
-  std::vector<TreeType> types;
+  std::vector<TreeType> types; //A reference Vector to tell what type the trees are
   std::map<std::string, Var> vars;
   unsigned place; //how many trees have been executed
   const ASGrove* parent;
@@ -35,12 +35,12 @@ public:
   ASGrove(std::vector<std::vector<Token>> commands, unsigned start, unsigned end, ASGrove* owner = nullptr);
   ASGrove(std::vector<std::vector<Token>> commands, ASGrove* owner = nullptr);
   ~ASGrove();
-  void update_existing(const std::map<std::string, Var>&);
+  void update_existing(const std::map<std::string, Var>&); //If a Variable exists both in the lower grove and upper grove, takes the value from the lower grove and assigns it to the upper grove. (Makes i++) 
   const std::map<std::string, Var>& show_vars() const;
-  Var eval();
-  Var calc(bool print = true);
+  Var eval(); // Evaluates all trees sequentially until the end
+  Var calc(bool print = true); //Stepper evaluates one tree one step at a time
   void add_tree(ASTree* tree, TreeType type = TreeType::EXP);
-  void reset();
+  void reset(); // Will reset the placement of the tree
   void print() const;
 };
 class StatementTree: public ASTree{
