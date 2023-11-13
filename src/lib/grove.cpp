e832fd7e55c1698aa1377724c879f5627894b044
Sawyer Rice <sawyerrice@ucsb.edu>
2023-11-13 14:35:10 -0800

    return printing

--- src/lib/grove.cpp
+++ src/lib/grove.cpp
@@ -594,7 +594,8 @@ void ASGrove::print(unsigned i, unsigned indent, ASTree* start) const{
 				}
 			}
 			if(fun->get_body()->statements.at(0)->getProot().get_pdata().get_type() == TokenType::VOID && fun->get_body()->statements.at(0)->getProot().get_pdata().get_text() == " "){
-				std::cout<<") {";
+				std::cout<<") {}";
+				break;
 			}else{
 				std::cout<<") {\n";
 			}
